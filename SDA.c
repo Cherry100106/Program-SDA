588048b1409fa0f87925f70d01a21552cddec9d4
byebay <bayuuua00@gmail.com>
2025-04-26 10:32:36 +0700

    HapusSemuaWarga sudah ada

---
+++ SDA.c
@@ -0,0 +1,12 @@
+#include sda.h
+
+void HapusSemuaWarga(Kota *kota) {
+    DataWarga *curr = kota->nextWarga;
+    while (curr != NULL) {
+        DataWarga *temp = curr;
+        curr = curr->next;
+        free(temp);
+    }
+    kota->nextWarga = NULL;
+}
+
