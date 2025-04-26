c9025794b4b3ba3b5d0ed6d7d889f1dfc9c47a11
byebay <bayuuua00@gmail.com>
2025-04-26 11:02:14 +0700

    HapusKota sudah ada

--- SDA.c
+++ SDA.c
@@ -10,3 +10,21 @@ void HapusSemuaWarga(Kota *kota) {
     kota->nextWarga = NULL;
 }
 
+void HapusKota(Kota daftarKota[], int *jumlahKota, const char *namaKota) {
+    for (int i = 0; i < *jumlahKota; i++) {
+        if (strcmp(daftarKota[i].NamaKota, namaKota) == 0) {
+            HapusSemuaWarga(&daftarKota[i]);
+
+            // geser geser
+            for (int j = i; j < *jumlahKota - 1; j++) {
+                daftarKota[j] = daftarKota[j + 1];
+            }
+            (*jumlahKota)--;
+
+            printf("Kota '%s' dan seluruh warganya berhasil dihapus.\n", namaKota);
+            return;
+        }
+    }
+    printf("Kota '%s' tidak ditemukan.\n", namaKota);
+}
+
