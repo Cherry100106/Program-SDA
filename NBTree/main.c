d8ea4c1b4c5182f080a8d7beab8d7ee4bdc63d53
byebay <bayuuua00@gmail.com>
2025-05-06 08:29:53 +0700

    Studi kasus

--- NBTree/main.c
+++ NBTree/main.c
@@ -1,27 +1,92 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "nbtrees.h"
 
 int main() {
     Isi_Tree pohon;
-    Create_tree(pohon, 10);
+    char cari;
+    char data1, data2;
+    int lvl;
+    int pilihan;
     
-    printf("Isi Tree:\n");
-    PrintTree(pohon);
+    Create_tree(pohon, jml_maks);
 
-    printf("\nPreOrder: ");
-    PreOrder(pohon);
+    do {
+        printf("\n=== MENU POHON NON-BINER ===\n");
+        printf("1. Traversal PreOrder\n");
+        printf("2. Traversal InOrder\n");
+        printf("3. Traversal PostOrder\n");
+        printf("4. Traversal LevelOrder\n");
+        printf("5. Print Tree\n");
+        printf("6. Search Node Tree\n");
+        printf("7. Jumlah Daun/Leaf\n");
+        printf("8. Mencari Level Node Tree\n");
+        printf("9. Kedalaman Tree\n");
+        printf("10. Membandingkan 2 Node Tree\n");
+        printf("11. Exit\n");
+        printf("Pilih Menu: ");
+        scanf("%d", &pilihan);
+        getchar();
 
-    printf("\nInOrder: ");
-    InOrder(pohon);
-
-    printf("\nPostOrder: ");
-    PostOrder(pohon);
-
-    printf("\nLevel Order: ");
-    LevelOrder(pohon, 10);
-
-    printf("\nDepth: %d\n", Depth(pohon));
-    printf("\nLevel dari simpul E: %d\n", Level(pohon, 'E'));
+        switch (pilihan) {
+            case 1:
+                printf("\nTraversal PreOrder: ");
+                PreOrder(pohon);
+                break;
+            case 2:
+                printf("\nTraversal InOrder: ");
+                InOrder(pohon);
+                break;
+            case 3:
+                printf("\nTraversal PostOrder: ");
+                PostOrder(pohon);
+                break;
+            case 4:
+                printf("\nTraversal LevelOrder: ");
+                LevelOrder(pohon, jml_maks);
+                break;
+            case 5:
+                printf("\nStruktur Tree:\n");
+                PrintTree(pohon);
+                break;
+            case 6:
+                printf("\nMasukkan huruf node yang dicari: ");
+                scanf(" %c", &cari);
+                if (Search(pohon, cari)) {
+                    printf("Node '%c' ditemukan.\n", cari);
+                } else {
+                    printf("Node '%c' tidak ditemukan.\n", cari);
+                }
+                break;
+            case 7:
+                printf("\nJumlah daun: %d\n", nbDaun(pohon));
+                break;
+            case 8:
+                printf("\nMasukkan node yang ingin dicari levelnya: ");
+                scanf(" %c", &cari);
+                lvl = Level(pohon, cari);
+                printf("Level dari node '%c' adalah: %d\n", cari, lvl);
+                break;
+            case 9:
+                printf("\nKedalaman tree: %d\n", Depth(pohon));
+                break;
+            case 10:
+                printf("\nMasukkan 2 node untuk dibandingkan (contoh: B J): ");
+                scanf(" %c %c", &data1, &data2);
+                if (!Search(pohon, data1) || !Search(pohon, data2)) {
+                    printf("Salah satu atau kedua node tidak ditemukan.\n");
+                } else {
+                    char maksimum = Max(data1, data2);
+                    printf("Node dengan nilai maksimum secara ASCII adalah: %c\n", maksimum);
+                }
+                break;
+            case 11:
+                printf("Keluar dari program.\n");
+                break;
+            default:
+                printf("Pilihan tidak valid. Silakan coba lagi.\n");
+        }
+    } while (pilihan != 11);
 
     return 0;
 }
